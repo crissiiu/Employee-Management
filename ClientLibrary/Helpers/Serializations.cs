62163cc63323edc7f4e791fe583caeca3099d1c0
crissiiu <crissiiu.dev@gmail.com>
2026-02-01 00:26:53 +0700

    Creating Service to Consume the API

---
+++ ClientLibrary/Helpers/Serializations.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+
+namespace ClientLibrary.Helpers
+{
+    public static class Serializations
+    {
+        //Chuyển một đối tượng C# thành chuỗi Json
+        public static string SerializeObj<T>(T modelObject) =>
+            JsonSerializer.Serialize(modelObject);
+
+        //Chuyển chuỗi Json thành một đối tượng C#
+        public static T DeserializeJsonString<T>(string jsonString) =>
+            JsonSerializer.Deserialize<T>(jsonString)!;
+        public static IList<T> DeserializeJsonStringList<T>(string jsonString) =>
+            JsonSerializer.Deserialize<IList<T>>(jsonString)!;
+
+    }
+}
